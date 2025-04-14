f82c437fa670a54465df1d8eefe3b7a5637eb624
Julho César Moreira <75630021+julhomoreira@users.noreply.github.com>
2025-04-14 18:32:16 +0100

    fashions 3
    fashions 3

---
+++ portfolio/fashion/project.js/js/calculator-modules/perform-calculations.js
@@ -0,0 +1,17 @@
+// perform-calculations.js
+
+// A função `calculate` será exportada
+export function calculate(firstOperand, secondOperand, operator) {
+    if (operator === '+') {
+      return firstOperand + secondOperand;
+    } else if (operator === '-') {
+      return firstOperand - secondOperand;
+    } else if (operator === '*') {
+      return firstOperand * secondOperand;
+    } else if (operator === '/') {
+      return firstOperand / secondOperand;
+    }
+  
+    return secondOperand;
+  }
+  
